95c14255e654b0e5e331bbf482172a09815ed0ee
John Politte <johncpolitte@yahoo.com>
2019-04-03 17:12:45 -0500

    move functions to py files

---
+++ df_builder_mini_cleaner.py
@@ -0,0 +1,47 @@
+import pymongo
+from pymongo import MongoClient
+import pandas as pd
+import numpy as np
+import re
+import itertools
+
+
+def cnn_cleaner(cnn_df):
+    '''
+    
+    '''
+    cnn_list = list(cnn_df.content)
+    clean_cnn_list = []
+    for article in cnn_list:
+        clean_article = re.sub('CNN', '', article)
+        clean_article2 = re.sub('[)(,.]', '', clean_article)
+        clean_cnn_list.append(clean_article2.lower())
+    clean_cnn_df = pd.DataFrame(clean_cnn_list, columns=['Article'])
+    clean_cnn_df['Satire'] = 0
+    clean_cnn_df['CNN'] = 1
+    clean_cnn_df['Fox'] = 0
+    return clean_cnn_df[0:5000]
+
+def fox_cleaner(fox_df):
+    fox_list = list(fox_df.content)
+    clean_fox_list = []
+    for article in fox_list:
+        clean = re.sub('[)(,.]', '', article)
+        clean_fox_list.append(clean.lower())
+    clean_fox_df = pd.DataFrame(clean_fox_list, columns=['Article'])
+    clean_fox_df['Satire'] = 0
+    clean_fox_df['CNN'] = 0
+    clean_fox_df['Fox'] = 1
+    return clean_fox_df
+
+def small_onion_clean(onion_df):
+    onion_df = onion_df.drop('Title', axis=1)
+    clean_onion_df = onion_df[0:5000]
+    return clean_onion_df[['Article','Satire','CNN', 'Fox']]
+
+def build_df(onion_df, fox_df, cnn_df):
+    df_onion = small_onion_clean(onion_df)
+    df_cnn = cnn_cleaner(cnn_df)
+    df_fox = fox_cleaner(fox_df)
+    df_final = pd.concat([df_onion, df_cnn, df_fox], axis = 0, ignore_index=True)
+    return df_final
